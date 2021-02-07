5ac0765b09130c4b3d6f754b71e6d6a53597343a
Kawtar Drissi el bouzaidi <kdrissi-@e1r2p6.1337.ma>
2021-02-07 19:06:41 +0100

    yes

--- parsing/get_map.c
+++ parsing/get_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_map.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: drissi <drissi@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kdrissi- <kdrissi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/27 16:06:18 by kdrissi-          #+#    #+#             */
-/*   Updated: 2021/02/07 00:02:02 by drissi           ###   ########.fr       */
+/*   Updated: 2021/02/07 16:23:42 by kdrissi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,48 +15,49 @@
 static int		g_p;
 static t_list	*g_head;
 
-void    print_map(void)
+void	print_map(void)
 {
-    int i = 0;
-    while(g_map[i])
-    {
-        printf("%s\n",g_map[i]);
-        i++;
-    }
-}
-int     map_first_digit(char *line)
-{
-    int i = 0; 
-    if(line[i] != '1' && line[i] != '0'&& line[i] != ' ')
-        return(0);
-    while(line[i] == ' ')
-        i++;
-    if(line[i] == '1'|| line[i] == '0')
-    {
-        return(1);
-    }
-    return(0);
+	int i = 0;
+	while (g_map[i])
+	{
+		printf("%s\n",g_map[i]);
+		i++;
+	}
 }
 
-void    map_debut(void)
+int		map_first_digit(char *line)
 {
-    g_head = g_file;
-    while(g_head)
-    {
-        if(map_first_digit((char *)g_head->content))
-            break;
-        g_head = g_head->next;
-    }
+	int i = 0; 
+	if(line[i] != '1' && line[i] != '0'&& line[i] != ' ')
+		return(0);
+	while(line[i] == ' ')
+		i++;
+	if(line[i] == '1'|| line[i] == '0')
+	{
+		return(1);
+	}
+	return(0);
+}
+
+void	map_debut(void)
+{
+	g_head = g_file;
+	while(g_head)
+	{
+		if(map_first_digit((char *)g_head->content))
+			break;
+		g_head = g_head->next;
+	}
 }
 
 
   
-void    map_size(void)
+void	map_size(void)
 {
-    t_list *temp;
+	t_list *temp;
 
-    temp = g_head;
-    while (temp)
+	temp = g_head;
+	while (temp)
 	{
 		if (map_first_digit(temp->content))
 		{
@@ -64,16 +65,16 @@ void    map_size(void)
 			if (ft_strlen(temp->content) > (size_t)g_columns)
 				g_columns = ft_strlen(temp->content);
 		}
-        else 
-            error("error map_size\n", NULL);
+		else 
+			error("error map_size\n", NULL);
 		temp = temp->next;
 	}
 }
 
-void    map_second_init(void)
+void    map_init(void)
 {
-    g_p = 0;
-    g_columns += 2;
+	g_p = 0;
+	g_columns += 2;
 	
 	if (!(g_map = (char **)malloc(sizeof(char *) * (g_rows + 1))))
 		error("error map_second_init maloc",NULL);
@@ -81,48 +82,49 @@ void    map_second_init(void)
 
 void    spacer(void)
 {
-    int     i;
-    i   =   0;
-    while(i <= g_rows)
-        {
-            if (!(g_map[i] = (char *)malloc(sizeof(char) * (g_columns + 1))))
-		        error("error spacer\n",NULL);
-            ft_memset(g_map[i], ' ', g_columns);
-            g_map[i][g_columns + 1] = '\0';
-            i++;
-        } 
-}
-
-void    map_filling(void)
-{ 
-    int i;
-    int j;
-    i = 1;
-
-
-    spacer();
-    while (g_head)
+	int     i;
+	
+	i   =   0;
+	while (i <= g_rows)
 	{
-        j = 0;
-		while (((char *)(g_head->content))[j])
-        {
-            g_map[i][j+1] = ((char*)(g_head->content))[j];
+		if (!(g_map[i] = (char *)malloc(sizeof(char) * (g_columns + 1))))
+			error("error spacer\n",NULL);
+		ft_memset(g_map[i], ' ', g_columns);
+		g_map[i][g_columns + 1] = '\0';
+		i++;
+	} 
+}
 
-            j++;
-        }
-    
+void	map_filling(void)
+{ 
+	int i;
+	int j;
+	i = 1;
+
+
+	spacer();
+	while (g_head)
+	{
+		j = 0;
+		while (((char *)(g_head->content))[j])
+		{
+			g_map[i][j+1] = ((char*)(g_head->content))[j];
+
+			j++;
+		}
+	
 		g_head = g_head->next;
-        i++;
+		i++;
 	}
-    g_map[g_rows] = NULL;
+	g_map[g_rows] = NULL;
 }
 
 
 
-int				map_errors(void)
+int		map_errors(void)
 {
 	int		j;
-    int     i;
+	int     i;
 
 	i = 1;
 	while (++i < g_rows)
@@ -130,31 +132,29 @@ int				map_errors(void)
 		j = -1;
 		while (++j < g_columns)
 		{
-			if (g_map[i][j] == 'N' || g_map[i][j] == 'S'|| g_map[i][j] == 'W' || g_map[i][j] == 'E')
+			if (ft_strchr("NSWE", g_map[i][j]))
 				g_p++;
 			if (ft_strchr("0NSWE2",g_map[i][j]))
 			{
-				if (g_map[i - 1][j] == ' ' || g_map[i][j - 1] == ' ' || g_map[i + 1][j] == ' ' || g_map[i][j + 1] == ' ')
-                {  printf("%d %d\n",i,j);
+				if (g_map[i - 1][j] == ' ' || g_map[i][j - 1] == ' ' ||
+					g_map[i + 1][j] == ' ' || g_map[i][j + 1] == ' ')
 					error("Error\n check spaces !\n", NULL);
-                }
 			}
 			else if (!(ft_strchr("0NSWE21 ",g_map[i][j])))
-            {
-                printf("%c %d %d",g_map[i][j],i,j);
-				error("Error\nMultiple or no players or Invalid map!\n", NULL);
-            }
+				error("Error\nMultiple or no players!\n", NULL);
 		}
 	}
+	if(g_p != 1)
+		error("error invalid number of players!\n", NULL);
 	return (0);
 }
 
 void    map_treatment(void)
 {
-    map_debut();;
-    map_size();
-    map_second_init();
-    map_filling();
-    print_map();
-    map_errors();
+	map_debut();;
+	map_size();
+	map_init();
+	map_filling();
+	map_errors();
+	ft_lst_clear(&g_file);	
 }
